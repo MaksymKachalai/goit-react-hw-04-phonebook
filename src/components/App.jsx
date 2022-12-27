3d7ff044bcff124ed78110a3a682b65ab16426c3
Maksym Kachalai <maks.kachalay@gmail.com>
2022-12-27 16:59:24 +0200

    refactor task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import ContactsList from './ContactsList/ContactsList';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +12,7 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      React homework template
+      <ContactsList />
     </div>
   );
 };
--- src/components/ContactsRendering/ContactsRendering.js
+++ src/components/ContactsRendering/ContactsRendering.js
@@ -1,25 +1,18 @@
-import React, { Component } from "react";
+import React from 'react';
 
-export class ContactsRendering extends Component {
-	render() {
-		const { filterList, deleteContact } = this.props;
-		return (
-			<>
-				<ul className="contactList">
-					{filterList.map((contact) => (
-						<li
-							key={contact.id}
-							className="contactList__item">
-							{contact.name} <span>{contact.number}</span>
-							<button
-								type="button"
-								onClick={() => deleteContact(contact.id)}>
-								Delete
-							</button>
-						</li>
-					))}
-				</ul>
-			</>
-		);
-	}
+export default function ContactsRendering({ filterList, deleteContact }) {
+  return (
+    <>
+      <ul className="contactList">
+        {filterList.map(contact => (
+          <li key={contact.id} className="contactList__item">
+            {contact.name} <span>{contact.number}</span>
+            <button type="button" onClick={() => deleteContact(contact.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
